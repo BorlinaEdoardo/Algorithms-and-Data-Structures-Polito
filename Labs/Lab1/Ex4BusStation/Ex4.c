40f8301423e74974bbe3e3b481fa835e0c87a473
BorlinaEdoardo <133708549+BorlinaEdoardo@users.noreply.github.com>
2024-08-01 19:07:14 +0200

    feat (lab1-Ex4): added r_print command functionalities

--- Labs/Lab1/Ex4BusStation/Ex4.c
+++ Labs/Lab1/Ex4BusStation/Ex4.c
@@ -5,7 +5,8 @@
 #include <string.h>
 
 #define LOG "corse.txt"
-#define COMMANDSNUM 7
+#define COMMANDSNUM 8
+#define DBG 1
 
 typedef struct route_s {
     char *id;
@@ -31,9 +32,9 @@ deposit_t newDeposit(){
     return  deposit;
 }
 
-typedef enum {r_help, r_date, r_partenza, r_capolinea, r_ritardo, r_ritardo_tot, r_fine}comando_e;
+typedef enum {r_help, r_date, r_partenza, r_capolinea, r_ritardo, r_ritardo_tot, r_print, r_fine}comando_e;
 
-char *commands[] = {"r_help","r_date", "r_partenza", "r_capolinea", "r_ritardo", "r_ritardo_tot", "r_fine"};
+char *commands[] = {"r_help","r_date", "r_partenza", "r_capolinea", "r_ritardo", "r_ritardo_tot", "r_print", "r_fine"};
 
 // function used to convert an input from string to comando_e
 comando_e commandSearch(char* input);
@@ -59,17 +60,46 @@ void freeDeposit(deposit_t d){
     d.maxSize = 0;
 }
 
+void printLine(route_t line, FILE *fd){
+    if(fd == NULL) return;
+    fprintf(fd, "%s %s %s %s %s %s %d\n",
+            line.id, line.departure, line.destination,
+            line.date, line.depTime, line.arrTime, line.delay);
+
+}
+
+// this function handles all implemented commands
+void commandHandling(deposit_t deposit, comando_e command);
+
 
 int main(){
     deposit_t deposit = newDeposit();
     comando_e command;
+    char line[100];
+
+    // loadings lines from file
+    FILE *fd;
+    fd = fopen(LOG, "r");
+    fscanf(fd, "%d\n", &deposit.maxSize);
+    deposit.lines = (route_t *) malloc(deposit.maxSize * sizeof(route_t));
+    for (int i = 0; i < deposit.maxSize; i++) {
+        fgets(line, 100, fd);
+        #if DBG
+            printf("%s", line);
+        #endif
+        addLine(&deposit, line);
+    }
+    fclose(fd);
+
+    // commands handling
+    printMenu();
     char input[30];
     do{
-        printMenu();
-        printf("\tcommand >_");
+        printf("@command >_");
         fflush(stdout);
         scanf("%s", input);
         command = commandSearch(input);
+        if(command != r_fine) commandHandling(deposit, command);
     } while(command != r_fine);
     printf("goodbye\n");
     freeDeposit(deposit);
@@ -87,7 +117,7 @@ comando_e commandSearch(char* input){
 }
 
 void printMenu(){
-    printf("-----------------------------------------------------------------\n"
+    printf("\n-----------------------------------------------------------------\n"
            "Available commands:\n"
            "\tr_help: print available commands\n"
            "\tr_date: shows all trips that departed between two dates\n"
@@ -95,6 +125,7 @@ void printMenu(){
            "\tr_capolinea: shows all lines ending at a specified stop\n"
            "\tr_ritardo: displays the delay for a specific route\n"
            "\tr_ritardo_tot: displays the total delay accumulated\n"
+           "\tr_print: print all existing lines\n"
            "\tr_fine: ends the program\n"
            "-----------------------------------------------------------------\n");
 }
@@ -102,7 +133,7 @@ void printMenu(){
 void addLine(deposit_t *deposit, char *line){
     route_t r;
     char id[30], dep[30], dest[30] , date[30], arrTime[30], depTime[30];
-    sscanf("%s %s %s %s %s %s %s %d", id, dep, dest, date, arrTime, depTime, r.delay);
+    sscanf(line, "%s %s %s %s %s %s %d\n", id, dep, dest, date, arrTime, depTime, &r.delay);
     r.id = strdup(id);
     r.departure = strdup(dep);
     r.destination = strdup(dest);
@@ -111,4 +142,45 @@ void addLine(deposit_t *deposit, char *line){
     r.arrTime = strdup(arrTime);
 
     deposit -> lines[(deposit->size)++] = r;
+}
+
+
+void commandHandling(deposit_t deposit, comando_e command){
+    switch (command) {
+        // r_help
+        case r_help:
+            printMenu();
+            break;
+
+        // r_date: shows all trips that departed between two dates
+        case r_date:
+            break;
+
+        // r_partenza: shows all routes starting from a specified stop
+        case r_partenza:
+            break;
+
+        // r_capolinea: shows all lines ending at a specified stop
+        case r_capolinea:
+            break;
+
+        // r_ritardo: displays the delay for a specific route
+        case r_ritardo:
+            break;
+
+        // r_ritardo_tot: displays the total delay accumulated
+        case r_ritardo_tot:
+            break;
+
+        //  r_print: print all existing lines
+        case r_print:
+            for (int i = 0; i < deposit.size; ++i) {
+                printLine(deposit.lines[i],stdout);
+            }
+            break;
+
+        default:
+            printf("the specified command does not exists\n");
+
+    }
 }
